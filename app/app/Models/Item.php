03509e133559ecba916b706bcdef3cfdecc059e1
KawataniShinya <peritrochoidonsilverfox@gmail.com>
2023-06-26 23:15:23 +0900

    add laravel-ide-helper

--- app/app/Models/Item.php
+++ app/app/Models/Item.php
@@ -5,6 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Item
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $memo
+ * @property int $price
+ * @property int $is_selling
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\ItemFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Item newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Item newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Item query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Item whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Item whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Item whereIsSelling($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Item whereMemo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Item whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Item wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Item whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Item extends Model
 {
     use HasFactory;
