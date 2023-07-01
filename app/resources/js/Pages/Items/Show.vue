4708464ca7eabd540f9ccca4e320cba69d2d3cc5
KawataniShinya <peritrochoidonsilverfox@gmail.com>
2023-07-01 12:53:01 +0900

    add handle line feed code on item memo

--- app/resources/js/Pages/Items/Show.vue
+++ app/resources/js/Pages/Items/Show.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import { nl2br } from "@/common";
 
 defineProps({
     item: Object
@@ -37,9 +38,7 @@ defineProps({
                                             <div class="p-2 w-full">
                                                 <div class="relative">
                                                     <label for="memo" class="leading-7 text-sm text-gray-600">メモ</label>
-                                                    <div id="memo" class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
-                                                        {{ item.memo }}
-                                                    </div>
+                                                    <div id="memo" v-html="nl2br(item.memo)" class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></div>
                                                 </div>
                                             </div>
                                             <div class="p-2 w-full">
