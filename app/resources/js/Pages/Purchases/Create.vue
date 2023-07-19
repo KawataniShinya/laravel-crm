dfb40bd30e84a3fbda9d7115ed9335ae06f09dff
KawataniShinya <peritrochoidonsilverfox@gmail.com>
2023-07-19 23:16:25 +0900

    add date input on purchase create

--- app/resources/js/Pages/Purchases/Create.vue
+++ app/resources/js/Pages/Purchases/Create.vue
@@ -1,6 +1,17 @@
 <script setup>
+import { getToday } from '@/common'
+import { useForm } from '@inertiajs/vue3';
+import { onMounted } from 'vue'
 
+onMounted(() => {
+    form.date = getToday()
+})
+
+const form = useForm({
+    date: null
+})
 </script>
 <template>
-    purchases/create
+    日付<br>
+    <input type="date" name="date" v-model="form.date">
 </template>
