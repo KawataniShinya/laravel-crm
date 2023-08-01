231d1360d150a1964a203ee85f1efe3ffb45d1ac
KawataniShinya <peritrochoidonsilverfox@gmail.com>
2023-08-01 20:39:39 +0900

    add search customer on modal window

--- app/app/Http/Controllers/PurchaseController.php
+++ app/app/Http/Controllers/PurchaseController.php
@@ -30,13 +30,11 @@ class PurchaseController extends Controller
      */
     public function create()
     {
-        $customers = Customer::select('id', 'name', 'kana')->get();
         $items = Item::select('id', 'name', 'price')->where('is_selling', true)->get();
 
         return Inertia::render(
             'Purchases/Create',
             [
-                'customers' => $customers,
                 'items' => $items
             ]
         );
