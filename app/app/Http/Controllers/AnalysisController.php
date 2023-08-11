e948eb8504c3f42c43dfc29ff05d6c53fc665ce5
KawataniShinya <peritrochoidonsilverfox@gmail.com>
2023-08-11 17:33:13 +0900

    add between date

--- app/app/Http/Controllers/AnalysisController.php
+++ app/app/Http/Controllers/AnalysisController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\In;
 use Inertia\Inertia;
@@ -10,6 +11,17 @@ class AnalysisController extends Controller
 {
     public function index()
     {
+        $startDate = '2023-07-01';
+        $endDate = '2023-07-31';
+
+        $period = Order::betweenDate($startDate, $endDate)
+            ->groupBy('id')
+            ->selectRaw('id, sum(subtotal) as total, customer_name, status, created_at')
+            ->orderBy('created_at')
+            ->paginate(50);
+
+        dd($period);
+
         return Inertia::render('Analysis');
     }
 }
