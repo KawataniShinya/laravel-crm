779cec4aaae9b8a00ab63866db0d22ed3e7e7859
KawataniShinya <peritrochoidonsilverfox@gmail.com>
2023-08-12 11:11:11 +0900

    add query total per purchase on analysis

--- app/app/Http/Controllers/AnalysisController.php
+++ app/app/Http/Controllers/AnalysisController.php
@@ -6,6 +6,7 @@ use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\In;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\DB;
 
 class AnalysisController extends Controller
 {
@@ -14,11 +15,23 @@ class AnalysisController extends Controller
         $startDate = '2023-07-01';
         $endDate = '2023-07-31';
 
-        $period = Order::betweenDate($startDate, $endDate)
+//        $period = Order::betweenDate($startDate, $endDate)
+//            ->groupBy('id')
+//            ->selectRaw('id, sum(subtotal) as total, customer_name, status, created_at')
+//            ->orderBy('created_at')
+//            ->paginate(50);
+
+        $subQuery = Order::betweenDate($startDate, $endDate)
+            ->where('status', true)
             ->groupBy('id')
-            ->selectRaw('id, sum(subtotal) as total, customer_name, status, created_at')
-            ->orderBy('created_at')
-            ->paginate(50);
+            ->selectRaw('id, sum(subtotal) as totalPerPurchase, DATE_FORMAT(created_at, "%Y%m%d") as date');
+
+        $data = DB::table($subQuery)
+            ->groupBy('date')
+            ->selectRaw('date, sum(totalPerPurchase) as total')
+            ->get();
+
+//        dd($data);
 
         return Inertia::render('Analysis');
     }
