632b04b67dff120ec60416650f3e953af5c50884
KawataniShinya <peritrochoidonsilverfox@gmail.com>
2023-08-12 15:45:54 +0900

    add test getting total by customer

--- app/app/Http/Controllers/AnalysisController.php
+++ app/app/Http/Controllers/AnalysisController.php
@@ -21,17 +21,29 @@ class AnalysisController extends Controller
 //            ->orderBy('created_at')
 //            ->paginate(50);
 
-        $subQuery = Order::betweenDate($startDate, $endDate)
-            ->where('status', true)
-            ->groupBy('id')
-            ->selectRaw('id, sum(subtotal) as totalPerPurchase, DATE_FORMAT(created_at, "%Y%m%d") as date');
-
-        $data = DB::table($subQuery)
-            ->groupBy('date')
-            ->selectRaw('date, sum(totalPerPurchase) as total')
-            ->get();
+//        $subQuery = Order::betweenDate($startDate, $endDate)
+//            ->where('status', true)
+//            ->groupBy('id')
+//            ->selectRaw('id, sum(subtotal) as totalPerPurchase, DATE_FORMAT(created_at, "%Y%m%d") as date');
+//
+//        $data = DB::table($subQuery)
+//            ->groupBy('date')
+//            ->selectRaw('date, sum(totalPerPurchase) as total')
+//            ->get();
 
 //        dd($data);
+
+        $subQuery = Order::betweenDate($startDate, $endDate)
+            ->groupBy('id')
+            ->selectRaw('id, customer_id, customer_name, SUM(subtotal) as totalPerPurchase');
+
+        $subQuery = DB::table($subQuery)
+            ->groupBy('customer_id')
+            ->selectRaw('customer_id, customer_name, SUM(totalPerPurchase) as total')
+            ->orderBy('total', 'desc')
+            ->get();
+
+        dd($subQuery);
 
         return Inertia::render('Analysis');
     }
