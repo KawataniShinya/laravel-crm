a942858749be1ae17fa45c047ec613cb8d436f9a
KawataniShinya <peritrochoidonsilverfox@gmail.com>
2023-08-12 15:57:48 +0900

    add count for decile analysis

--- app/app/Http/Controllers/AnalysisController.php
+++ app/app/Http/Controllers/AnalysisController.php
@@ -40,10 +40,34 @@ class AnalysisController extends Controller
         $subQuery = DB::table($subQuery)
             ->groupBy('customer_id')
             ->selectRaw('customer_id, customer_name, SUM(totalPerPurchase) as total')
-            ->orderBy('total', 'desc')
-            ->get();
+            ->orderBy('total', 'desc');
 
-        dd($subQuery);
+        DB::statement('set @row_num = 0;');
+        $subQuery = DB::table($subQuery)
+            ->selectRaw('
+                @row_num:= @row_num+1 as row_num,
+                customer_id,
+                customer_name,
+                total
+            ');
+
+
+
+        $count = DB::table($subQuery)->count();
+        $total = DB::table($subQuery)->selectRaw('SUM(total) as total')->get();
+        $total = $total[0]->total;
+
+        $decile = ceil($count / 10);
+
+        $bindValues = [];
+        $tempValue = 0;
+        for ($i=1; $i<=10; $i++) {
+            array_push($bindValues, 1 + $tempValue);
+            $tempValue += $decile;
+            array_push($bindValues, 1 + $tempValue);
+        }
+
+        dd($count, $decile, $bindValues);
 
         return Inertia::render('Analysis');
     }
