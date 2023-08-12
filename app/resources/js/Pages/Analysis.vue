f3344488de80749b6e34f4d42874d3c766980006
KawataniShinya <peritrochoidonsilverfox@gmail.com>
2023-08-12 11:48:37 +0900

    add table display total data to analysis

--- app/resources/js/Pages/Analysis.vue
+++ app/resources/js/Pages/Analysis.vue
@@ -1,7 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, useForm } from '@inertiajs/vue3';
-import { onMounted } from 'vue'
+import {onMounted, reactive} from 'vue'
 import { getToday } from '@/common'
 
 onMounted(() => {
@@ -15,6 +15,8 @@ const form = useForm({
     type: 'perDay'
 })
 
+const data = reactive({})
+
 const getData = async () => {
     try {
         await axios.get('/api/analysis/', {
@@ -24,6 +26,7 @@ const getData = async () => {
                 type: form.type
             }
         }).then( res => {
+            data.data = res.data.data
             console.log(res.data)
         })
     } catch (e) {
@@ -50,6 +53,22 @@ const getData = async () => {
                             To : <input type="date" name="endDate" v-model="form.endDate"><br>
                             <button class="mt-4 flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">分析する</button>
                         </form>
+                        <div v-show="data.data" class="lg:w-2/3 w-full mx-auto overflow-auto">
+                            <table class="table-auto w-full text-left whitespace-no-wrap">
+                                <thead>
+                                <tr>
+                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">年月日</th>
+                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">金額</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr v-for="item in data.data" :key="item.date">
+                                    <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.date }}</td>
+                                    <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.total }}</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
